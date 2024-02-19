fb9940bda19c0a8fa4e661d9092df7237f1b35dd
MattEqualsCoder <mattequalscoder@gmail.com>
2024-02-19 14:20:14 -0500

    Update to use Host.CreateDefaultBuilder

--- SnesConnectorApp/App.axaml.cs
+++ SnesConnectorApp/App.axaml.cs
@@ -2,8 +2,6 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
-using Serilog;
-using SnesConnectorLibrary;
 
 namespace SnesConnectorApp;
 
@@ -16,24 +14,9 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        Log.Logger = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .WriteTo.File("snes-connector-app_.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 30)
-            .WriteTo.Console()
-            .WriteTo.Debug()
-            .CreateLogger();
-        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var serviceCollection = new ServiceCollection()
-                .AddSnesConnectorServices()
-                .AddLogging(logging =>
-                {
-                    logging.AddSerilog(dispose: true);
-                })
-                .AddSingleton<MainWindow>();
-            var services = serviceCollection.BuildServiceProvider();
-            desktop.MainWindow = services.GetRequiredService<MainWindow>();
+            desktop.MainWindow = Program.MainHost?.Services.GetRequiredService<MainWindow>();
         }
 
         base.OnFrameworkInitializationCompleted();
--- SnesConnectorApp/Program.cs
+++ SnesConnectorApp/Program.cs
@@ -1,16 +1,45 @@
 ﻿using Avalonia;
 using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using SnesConnectorLibrary;
 
 namespace SnesConnectorApp;
 
 class Program
 {
+    internal static IHost? MainHost { get; private set; }
+    
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.File("snes-connector-app_.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 30)
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .CreateLogger();
+        
+        MainHost = Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+            .ConfigureLogging(logging =>
+            {
+                logging.AddSerilog(dispose: true);
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddSnesConnectorServices();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
